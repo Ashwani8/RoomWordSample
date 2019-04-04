e625f57c624de416ac4ce20828048ab8001b028e
Ashwani Kumar <akumar@monmouthcollege.edu>
2019-04-03 20:45:55 -0500

    Annotate the Word class

--- app/src/main/java/com/example/roomwordsample/Word.java
+++ app/src/main/java/com/example/roomwordsample/Word.java
@@ -1,12 +1,26 @@
 package com.example.roomwordsample;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
+// Annotate class Word to be useful to Roomdata base
+
+@Entity(tableName = "word_table")
+// if we did not provide table name, class name will be used as default
 public class Word {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word") // specify the name of the column
     private String mWord;
+
+    // Constructor
     public Word(@NonNull String word){
         this.mWord = word;
     }
+
+    // Getter method
     public String getWord(){
         return this.mWord;
     }
