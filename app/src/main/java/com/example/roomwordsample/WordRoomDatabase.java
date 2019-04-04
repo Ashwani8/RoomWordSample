5b9a52fdc504f7c5573155241115173c3451ad30
Ashwani Kumar <akumar@monmouthcollege.edu>
2019-04-04 12:13:36 -0500

    Add a Room Database

---
+++ app/src/main/java/com/example/roomwordsample/WordRoomDatabase.java
@@ -0,0 +1,35 @@
+package com.example.roomwordsample;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+// Room is a database layer on top of an SQLite database. it must be abstract and extended RoomDatabse
+
+@Database(entities = {Word.class}, version = 1, exportSchema = false)
+public abstract class WordRoomDatabase extends RoomDatabase {
+    public abstract WordDao wordDao(); // provide getter method for each Dao
+
+    // Create a WordRoomDatabase as a singleton to prevent having multiple instances
+    // of the data base opened at the same time
+    private static WordRoomDatabase INSTANCE;
+    public static WordRoomDatabase getDatabase(final Context context){
+
+        if(INSTANCE == null){
+            synchronized (WordRoomDatabase.class){
+                if(INSTANCE == null){
+                    // Create database here using Room's database builder to create
+                    // a Roomdatabase object named "word_database" in the application context
+                    // from the WordRoomDatabase class
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database")
+                            // Wipes and rebuilds instead of migrating if no Migration object.
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
